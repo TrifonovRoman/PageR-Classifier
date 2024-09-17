de10407e7b2b3287920c72274e6b67b34d983c65
TrifonovRoman <gromtr3@gmail.com>
2024-09-17 18:50:11 +0800

    create base extractor

--- PageR-Classifier/scr/base-classifier.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-sys.path.append(os.path.abspath('../PageR-main'))
-from pager.page_model.sub_models.base_sub_model import BaseExtractor, BaseSubModel
-class BaseClassifier(BaseExtractor):
-    def extract(self, model: BaseSubModel) -> None:
-        for block in model.blocks:
-            block.set_label("text")
