2867c9d9591260dca44fd85df191aa800ec39caa
TrifonovRoman <gromtr3@gmail.com>
2024-09-17 19:11:03 +0800

    pager as library

--- scr/base-classifier.py
+++ scr/base-classifier.py
@@ -1,7 +1,4 @@
-import sys
-import os
-sys.path.append(os.path.abspath('../PageR-main'))
-from pager.page_model.sub_models.base_sub_model import BaseExtractor, BaseSubModel
+from pager import BaseExtractor, BaseSubModel
 class BaseClassifier(BaseExtractor):
     def extract(self, model: BaseSubModel) -> None:
         for block in model.blocks:
