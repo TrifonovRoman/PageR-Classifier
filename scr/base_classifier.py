ac276dbf2e72d5a2d5bc329ec018d82ea4110dc7
TrifonovRoman <gromtr3@gmail.com>
2024-10-01 20:51:13 +0800

    added the _classification(graph) method

--- scr/base_classifier.py
+++ scr/base_classifier.py
@@ -1,5 +1,13 @@
 from pager import BaseExtractor, BaseSubModel
+import tensorflow as tf
 class BaseClassifier(BaseExtractor):
+    def __init__(self, save_path: str):
+        super().__init__()
+        self.mlp_loaded = tf.saved_model.load(save_path)
     def extract(self, model: BaseSubModel) -> None:
-        for block in model.blocks:
-            block.label = "text"
+        for block, graph in zip(model.blocks,model.graphs):
+            block.label = self._classification(graph)
+
+    def _classification(self, graph):
+        result = self.mlp_loaded(graph)
+        return "текст" if result > 0.5 else "заголовок"
