e26b70ddf56051a64f45eebdad0a27f2ff3b6d49
TrifonovRoman <147297624+TrifonovRoman@users.noreply.github.com>
2024-12-23 19:26:59 +0800

    Delete scr/gnn_phisical_model.py

--- scr/gnn_phisical_model.py
+++
@@ -1,7 +0,0 @@
-from pager import PhisicalModel
-from typing import List, Tuple
-import numpy as np
-class GNNPhisicalModel(PhisicalModel):
-    def __init__(self) -> None:
-        super().__init__()
-        self.graphs: List[Tuple[np.ndarray, np.ndarray, np.ndarray]] = []
